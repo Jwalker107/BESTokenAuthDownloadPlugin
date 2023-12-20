84faf3be21aa6ab44baf0a033184c5874b6e3558
Jason Walker <jason.walker@hcl.com>
2023-12-19 20:33:52 -0600

    work-in-progress

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AuthDownloadPlugin
+# TokenAuthDownload
 BigFix Download Plug-In for Authenticated HTTPS downloads using token authentication (i.e. GitHub)
 
 To build the plugin (assuming Python is already installed)
@@ -7,22 +7,22 @@ To build the plugin (assuming Python is already installed)
 * Test script loads
   - python TokenAuthDownload.py -h
 * Create executable
-  - pyinstaller TokenAuthDownload.py
-  - generates build\TokenAuthDownload\TokenAuthDownload.exe
+  - pyinstaller --onefile TokenAuthDownload.py
+  - generates dist\TokenAuthDownload\TokenAuthDownload.exe
 
 
 To Load Plugin on the BES Server:
 
-Create json install file, ex. "plugin_TokenAuthDownload":
+Create json install file, ex. "plugin_TokenAuthDownload" (filename should begin with 'plugin_' and have no filename extension):
 
     {
        "message" : "add",
        "protocol" : "TokenAuthDownload",
-       "location" : "C:\\Program Files (x86)\\BigFix Enterprise\\BES Server\\DownloadPlugins\\TokenDownloadPlugin\\TokenDownloadPlugin.exe"
+       "location" : "C:\\Program Files (x86)\\BigFix Enterprise\\BES Server\\DownloadPlugins\\TokenAuthDownload\\TokenAuthDownload.exe"
     }
 
 Place the file in C:\Program Files (x86)\BigFix Enterprise\BES Server\Mirror Server\Inbox.  The file is ingested by the BESRootServer and will be deleted from this directory when processed.
-Create the target directory (`C:\Program Files (x86)\BigFix Enterprise\BES Server\DownloadPlugins\TokenDownloadPlugin` ) and copy `build\TokenDownloadPlugin\TokenDownloadPlugin.exe` to that directory.
+Create the target directory (`C:\Program Files (x86)\BigFix Enterprise\BES Server\DownloadPlugins\TokenAuthDownload` ) and copy `dist\TokenAuthDownload\TokenAuthDownload.exe` to that directory.
 
 To remove the download plugin, create file "plugin_TokenAuthDownload" and place in the Mirror Server\Inbox directory:
 
@@ -34,9 +34,10 @@ To remove the download plugin, create file "plugin_TokenAuthDownload" and place 
 
 To configure the plugin, 
 * create an authentication token (assuming github.com, select your profile -> Settings -> Developer Options -> Personal Access Tokens).
+* TODO - must run as the BESRootServer service account! or psexec -i -s cmd.exe !
 * Execute TokenAuthDownload.exe --set_token
 * When prompted, paste the Personal Access Token into the input field (the token will not be displayed on the screen)
 * If you wish to remove the saved token later, delete it using the Windows Credential Manager interface
 
 To use the plugin, create a download action message such as
-`prefetch MyReadme.md size:79 sha1:f919f61f325ff604e9359f8f448d3d1120cc81f2 url:TokenAuthDownload://raw.githubusercontent.com/Jwalker107/AuthDownloadPlugin/main/README.md sha256:82c9427a5aa78a76c5c89d003427b8f692a45a91c7aa50a09b93bb1cf3ace8d7`
+`prefetch MyReadme.md sha1:f919f61f325ff604e9359f8f448d3d1120cc81f2 size:79 TokenAuthDownload://raw.githubusercontent.com/Jwalker107/AuthDownloadPlugin/main/README.md sha256:82c9427a5aa78a76c5c89d003427b8f692a45a91c7aa50a09b93bb1cf3ace8d7`
--- TokenAuthDownload.py
+++ TokenAuthDownload.py
@@ -10,6 +10,24 @@ import argparse
 import keyring
 import requests
 import getpass
+import logging
+
+def init_logging(logfile:str, level:int=20 ):
+    # Creates the global 'logging' module and 'statuslog' variable - for debug and status logs
+    # Creates the directories if they do not exist
+    # Logs are created in the 'log' directory relative to the running script
+  
+    # FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
+    logging.basicConfig(
+        # level=logging.info,
+        # Default log level is INFO but overridden by the config file after it is read
+        level=level,
+        format="[%(asctime)s] %(funcName)20s() [%(levelname)s] %(message)s",
+        handlers=[logging.FileHandler(logfile), logging.StreamHandler()],
+    )
+    logging.info('Logging started to "%s"', logfile)
+
+
 
 def download_file_stream(
     session=requests.Session(),
@@ -38,10 +56,10 @@ def download_file_stream(
                     f.flush()
                     os.fsync(f.fileno())
 
-def get_downloads(filepath):
+def get_options(filepath):
     with open(filepath, "r") as file:
-        downloads = json.load(file)
-    return downloads
+        options = json.load(file)
+    return options
 
 def sendResults(results, options):
     message = {}
@@ -49,16 +67,19 @@ def sendResults(results, options):
     message["id"] = options["id"]
     message["status"] = results
         
-    status_filepath=tempfile.NamedTemporaryFile(
-            delete=False,
-            dir=options["inbox"],
-            prefix="plugin_" + str(options["id"]) + "_"
-        ).name
-    
+    # status_filepath=tempfile.NamedTemporaryFile(
+    #         delete=False,
+    #         dir=options["inbox"],
+    #         prefix="plugin_" + str(options["id"]) + "_"
+    #     ).name
+    plugin_id=options['id']
+    inbox_path=options['inbox']
+    results_file=os.path.join(inbox_path, f"plugin_{plugin_id}")
+    logging.info(f'Saving results to file {results_file} : {str(message)}')
     with open(
-        status_filepath, "w"
+        results_file, "w"
     ) as status_file:
-        json.dump(message, status_file)
+        json.dump(results, status_file)
 
 def prompt_password(prompt="Enter password:", confirm=None):
     # Prompts the user to enter a password
@@ -80,51 +101,75 @@ def get_args(argv=sys.argv[1:]):
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("--downloads", "-d", type=str)
     group.add_argument("--set_token", "-s", help="Save authentication token to keyring", action="store_true")
+    parser.add_argument("--verbose", "-v", action="store_true")
     args=parser.parse_args(argv)
     return args
 
 
 def main():
-  
+    plugin_system_name = "TokenAuthDownload"
+
+        # If the application is run as a bundle, the PyInstaller bootloader
+        # # extends the sys module by a flag frozen=True and sets the app
+        # # path into variable _MEIPASS'.
+        # print("Using frozen config")
+    if getattr(sys, "frozen", False):
+ 
+        scriptPath = os.path.dirname(os.path.abspath(sys.executable))
+    else:
+        scriptPath = os.path.dirname(os.path.abspath(__file__))
+    init_logging( os.path.join(scriptPath, 'logfile.txt') )
+    
+    ##############
     # examples of simulated command-line arguments, for use in a python debugger:
     # args=get_args(["--set_token"])
 
+    
     # args = get_args(
-    #     ["--downloads", os.path.join(os.path.dirname(os.path.realpath(__file__)), "downloads.json"), "--verbose"]
+    #     ["--downloads"
+    #      , os.path.join(os.path.dirname(os.path.realpath(__file__)), "downloads.json")
+    #      , "--verbose"
+    #      ]
     # )
 
-    # execute using the default command-line arguments
+    #execute using the default command-line arguments
     args=get_args()
+    ##################    
+    if args.verbose:
+        logging.getLogger().setLevel(10)
 
     if args.set_token:
         keyring.set_password(
-            "TokenDownloadPlugin",
+            plugin_system_name,
             "",
             prompt_password("Enter Token to store:"),
         )
-        
-     
-    plugin_system_name = "TokenAuthDownload"
     
     if args.downloads is not None:
-        token_container=keyring.get_credential("TokenDownloadPlugin", "")
+        logging.info(f"Processing downloads from {args.downloads}")
+        logging.info(f"Retrieving keyring credential for {plugin_system_name}")
+        token_container=keyring.get_credential(plugin_system_name, "")
         if token_container is None:
             raise ValueError("No stored token was found, try --set_token")
         token=token_container.password
 
-        downloads_listing = get_downloads(args.downloads)
-        results = []
+        options = get_options(args.downloads)
 
+        results = []
+        logging.info("Creating requests.Session")
         session = requests.Session()
         session.headers.update({"User-Agent": "Wget/1.14 (linux-gnu)"})
         session.headers.update({"Authorization": f"token {token}"})
 
         # This is tuned for ease-of-use rather than performance.
         # Currently each download is performed sequentially, not in parallel
-        for download in downloads_listing.get("downloads", {}):
+        for download in options.get("downloads", {}):
             result = {}
             result["id"] = download["id"]
+            logging.info(f"Processing download id {result['id']}")
             url = download.get("url").replace(plugin_system_name, "https")
+            logging.info(f"Download URL: {url}")
+            logging.info(f"Output file: {download.get('file')}")
             # download_file_stream will raise an exception on HTTP errors in addition to connection errors
             # so any response other than 'ok' will be caught by this exception handler
             try:
@@ -138,11 +183,16 @@ def main():
                 result["error"] = None
 
             except Exception as e:
+                logging.info(f"Download failed with {str(e)}")
                 result["success"] = False
                 result["error"] = str(e)
             results.append(result)
         # Send download status results to the downloads_listing file, where it will be read by the Server to provide action status / error messages to the console
-        sendResults(results, downloads_listing)
+        logging.info(f"Results: {str(results)}")
+        
+        logging.info(f"Preparing results to {results}")
+        sendResults(results, options)
+    logging.info("Plugin finished")
 
 if __name__ == "__main__":
     main()
